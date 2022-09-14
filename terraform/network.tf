fc7e86583e4b1806d15aa4061da2788ab5e61c37
QJoly <quentinjoly@thoughtless.eu>
2022-09-14 11:22:17 +0200

    Usage d'un module

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,12 +1,8 @@
-resource "libvirt_network" "manynodes_net" {
-  name      = var.network_name
-  domain    = var.domain
-  mode      = "nat"
-  addresses = var.subnet_cidr
-  dhcp {
-    enabled = true
-  }
-  dns {
-    enabled = true
-  }
+module "manynodes_net" {
+  source = "./modules/network"
+  name = var.network_name  
+  domain = var.domain
+  subnet_cidr = var.subnet_cidr
+  # dhcp = true  # enabled if not precised
+  # dns = true   # enabled if not precised
 }
